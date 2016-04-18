94e2b40795af672198ad08f387464037f8327418
jon-wade <jonwadeuk@gmail.com>
2016-04-18 09:46:47 +0100

    add in form validation on total meal price field

--- js/myApp.js
+++ js/myApp.js
@@ -1,5 +1,12 @@
 //main body goes here
 angular.module('myApp', [])
     .controller('myCtrl', function($scope){
+        $scope.baseMealPrice;
+
+
+        $scope.submit = function(){
+            console.log($scope.mealInput);
+            console.log($scope.mealInput.baseMealPrice.$error)
+        }
 
     });
