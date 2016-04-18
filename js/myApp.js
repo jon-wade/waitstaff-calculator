591530cc3ac89ff7c8c72e0b82a3eba8dabeb462
jon-wade <jonwadeuk@gmail.com>
2016-04-18 09:53:29 +0100

    add form validation for tax rate percentage

--- js/myApp.js
+++ js/myApp.js
@@ -2,11 +2,13 @@
 angular.module('myApp', [])
     .controller('myCtrl', function($scope){
         $scope.baseMealPrice;
+        $scope.taxRate;
 
 
         $scope.submit = function(){
             console.log($scope.mealInput);
             console.log($scope.mealInput.baseMealPrice.$error)
+            console.log($scope.mealInput.taxRate.$error)
         }
 
     });
