6e1fc3f1d665ea0e57c45b7c7285051e8362e5b0
jon-wade <jonwadeuk@gmail.com>
2016-04-18 10:22:00 +0100

    include logic to work out customer bill

--- js/myApp.js
+++ js/myApp.js
@@ -3,18 +3,31 @@ angular.module('myApp', [])
     .controller('myCtrl', function($scope){
         $scope.baseMealPrice;
         $scope.taxRate;
-        $scope.tipPecentage;
+        $scope.tipPercentage;
         $scope.err=false;
+        $scope.subTotal=0;
+        $scope.tip=0;
+        $scope.customerTotal=0;
 
 
         $scope.submit = function(){
             if ($scope.mealInput.$invalid){
                 $scope.err=true;
             }
+            else {
+
+                $scope.subTotal = ($scope.baseMealPrice*(1+($scope.taxRate/100)));
+                $scope.tip = ($scope.baseMealPrice*($scope.tipPercentage/100));
+                $scope.customerTotal = $scope.subTotal + $scope.tip;
+            }
+
+
+
+
             console.log($scope.mealInput);
-            console.log($scope.mealInput.baseMealPrice.$error)
-            console.log($scope.mealInput.taxRate.$error)
-            console.log($scope.mealInput.tipPecentage.$error)
+            console.log($scope.mealInput.baseMealPrice.$error);
+            console.log($scope.mealInput.taxRate.$error);
+            console.log($scope.mealInput.tipPercentage.$error);
         }
 
     });
