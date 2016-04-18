7d6866bc60ad3286975ee46e0a21c97466346e9d
jon-wade <jonwadeuk@gmail.com>
2016-04-18 10:01:40 +0100

    add in form validation for tip percentage and show-hide error logic

--- js/myApp.js
+++ js/myApp.js
@@ -3,12 +3,18 @@ angular.module('myApp', [])
     .controller('myCtrl', function($scope){
         $scope.baseMealPrice;
         $scope.taxRate;
+        $scope.tipPecentage;
+        $scope.err=false;
 
 
         $scope.submit = function(){
+            if ($scope.mealInput.$invalid){
+                $scope.err=true;
+            }
             console.log($scope.mealInput);
             console.log($scope.mealInput.baseMealPrice.$error)
             console.log($scope.mealInput.taxRate.$error)
+            console.log($scope.mealInput.tipPecentage.$error)
         }
 
     });
